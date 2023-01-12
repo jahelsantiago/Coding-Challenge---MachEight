8fe7f99aad0d4fd64dc2cdf2138dc3631129f33e
Jahel_Leon <jleonro@unal.edu.co>
2023-01-12 18:03:12 -0500

    added readme and main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from pair_sums import pair_sums
+
 #write the main function
 def main():
-    #write your code below this line
-    pass
+    #read the input
+    input_text = input("> app ")
+    nums, target = input_text.split(" ")
+    #cast the input
+    nums = [int(n) for  n in nums.split(",")]
+    target = int(target)
+    #solve the problem
+    pair_sums_list = pair_sums(nums, target)
+    #print the output
+    for pair in pair_sums_list:
+        print(f'+ {pair[0]},{pair[1]}')
+
+
 
 if __name__ == '__main__':
     main()
---
+++ readme.md
@@ -0,0 +1,38 @@
+
+# Coding Challenge - MachEight
+
+
+The algorithms is based on Two pointers tecnic and runs in n*log(n)
+
+
+## Run Locally
+
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+To run the test
+
+```bash
+  python test_pair_sums.py
+```
+
+Run the terminal version
+
+```bash
+  npm run start
+```
+
+Write a list of numbers to search separated by coma and the targert sperated by space
+
+```bash
+  > app 1,9,5,0,20,-4,12,16,7 12
+```
+
